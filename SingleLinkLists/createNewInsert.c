ee5a54d722aa3cc669f9ba62caf821d89136bf60
Roohi <syedaroohinaz627@gmail.com>
2025-07-12 12:15:48 +0530

    new file added

---
+++ SingleLinkLists/createNewInsert.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* link;
+};
+
+void insertatend(struct node **head, int data)
+{
+    
+    struct node* newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->link = NULL;
+    
+    if(*head == NULL)
+    {
+        *head = newnode;
+        return;
+    }
+    
+    struct node * temp = *head;
+    while(temp->link !=NULL)
+    {
+        temp = temp->link;
+    }
+    
+    temp->link = newnode;
+}
+
+void print(struct node* head)
+{
+    while(head!=NULL)
+    {
+        printf("%d->",head->data);
+        head = head->link;
+    }
+}
+
+int main()
+{
+  struct node* head  = NULL;
+  
+  insertatend(&head,10);
+  insertatend(&head,20);
+  insertatend(&head,30);
+  insertatend(&head,40);
+  
+  
+  print(head);
+    
+    
+    
+    
+
+    return 0;
+}
